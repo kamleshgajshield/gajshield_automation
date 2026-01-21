1b93e17e9240e705171e0cdccb4673b106c7665b
Kamlesh <kamlesh@gajshield.com>
2026-01-21 10:34:44 +0530

    update code

--- tech.py
+++ tech.py
@@ -1,4 +1,18 @@
+# factorial number
 a=1
 for i in range(1,6):
     a=a*i
-print("this is factorial number",a)    
+print("this is factorial number",a)   
+
+
+#fibbonacci series
+a=5
+x=0
+y=1
+z=0
+while a>=z:
+    x=y
+    y=z
+    z=x+y
+print("fibbonacci series",z)    
+
